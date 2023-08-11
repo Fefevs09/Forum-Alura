46236958160461a79cf3056fe48d7c52f521e6d8
Felipe Vieira <felipefernandes.contato09@gmail.com>
2023-08-11 00:26:57 -0300

    include attributes in data class Topico

--- src/main/kotlin/br/com/alura/forum/model/Topico.kt
+++ src/main/kotlin/br/com/alura/forum/model/Topico.kt
@@ -1,4 +1,17 @@
 package br.com.alura.forum.model
 
-class Topico {
-}
+import org.springframework.web.bind.annotation.ModelAttribute
+import java.time.LocalDateTime
+
+data class Topico (
+    val id: Long? = null,
+    val titulo: String,
+    val mensagem: String,
+    val dataCriacao: LocalDateTime = LocalDateTime.now(),
+    val curso: Curso,
+    val autor: Usuario,
+    val status: StatusTopico = StatusTopico.NAO_RESPONDIDO,
+    val resposta: List<Resposta> = ArrayList()
+
+
+)
