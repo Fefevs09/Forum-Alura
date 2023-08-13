443044f40733728e91eb206ca8d580a2e2270b2c
Felipe Vieira <felipefernandes.contato09@gmail.com>
2023-08-13 12:12:54 -0300

    refactor code add mappers

--- src/main/kotlin/br/com/alura/forum/controller/TopicoController.kt
+++ src/main/kotlin/br/com/alura/forum/controller/TopicoController.kt
@@ -1,15 +1,10 @@
 package br.com.alura.forum.controller
 
-import br.com.alura.forum.model.Curso
+import br.com.alura.forum.dto.NovoTopicoForm
+import br.com.alura.forum.dto.TopicoView
 import br.com.alura.forum.model.Topico
-import br.com.alura.forum.model.Usuario
 import br.com.alura.forum.service.TopicoService
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
-import java.util.*
+import org.springframework.web.bind.annotation.*
 
 
 @RestController
@@ -17,13 +12,18 @@ import java.util.*
 class TopicoController(private val service: TopicoService){
 
     @GetMapping
-    fun listar():List<Topico> {
+    fun listar():List<TopicoView> {
      return service.listar()
     }
 
 
     @GetMapping("/{id}")
-    fun buscarPorId(@PathVariable id: Long): Topico {
+    fun buscarPorId(@PathVariable id: Long): TopicoView {
        return service.buscarPorId(id)
+    }
+
+    @PostMapping
+    fun cadastrar(@RequestBody dto: NovoTopicoForm) {
+        service.cadastrar(dto)
     }
 }
--- src/main/kotlin/br/com/alura/forum/service/TopicoService.kt
+++ src/main/kotlin/br/com/alura/forum/service/TopicoService.kt
@@ -1,14 +1,23 @@
 package br.com.alura.forum.service
 
+import br.com.alura.forum.dto.NovoTopicoForm
+import br.com.alura.forum.dto.TopicoView
+import br.com.alura.forum.mapper.TopicoFormMapper
+import br.com.alura.forum.mapper.TopicoViewMapper
 import br.com.alura.forum.model.Curso
 import br.com.alura.forum.model.Topico
 import br.com.alura.forum.model.Usuario
 import org.springframework.stereotype.Service
 import java.util.*
+import java.util.stream.Collectors
 
 
 @Service
-class TopicoService(private var topicos: List<Topico>) {
+class TopicoService(
+    private var topicos: List<Topico>,
+    private val topicoViewMapper: TopicoViewMapper,
+    private val topicoFormMapper: TopicoFormMapper
+) {
     init {
         val topico = Topico(
             id = 1,
@@ -61,14 +70,24 @@ class TopicoService(private var topicos: List<Topico>) {
         topicos = Arrays.asList(topico, topico2, topico3)
     }
 
-    fun listar():List<Topico> {
-        return topicos
+    fun listar(): List<TopicoView> {
+        return topicos.stream().map {
+            t -> topicoViewMapper.map(t)
+        }.collect(Collectors.toList())
     }
 
-    fun buscarPorId(id: Long): Topico {
-        return topicos.stream().filter({
+    fun buscarPorId(id: Long): TopicoView {
+        val topico =  topicos.stream().filter {
             t -> t.id == id
-            }).findFirst().get()
+        }.findFirst().get()
 
+        return topicoViewMapper.map(topico)
+
+    }
+
+    fun cadastrar(form: NovoTopicoForm) {
+        val topico = topicoFormMapper.map(form)
+        topico.id = topicos.size.toLong() + 1
+        topicos = topicos.plus(topico)
     }
 }
